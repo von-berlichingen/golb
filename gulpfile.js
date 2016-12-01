6685090744321a41a117980aa777ff576c8d2cfb
von-berlichingen <anis.b.amara@gmail.com>
2016-12-02 00:11:13 +0100

    Gulp_3 Moving files with Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 
 gulp.task('default', [], function() {
-  console.log('Hello Gulp!');
+  console.log('Moving all files in styles folder!');
+  gulp
+    .src('contents/styles/**.*')
+    .pipe(gulp.dest('build/styles'));
 });
