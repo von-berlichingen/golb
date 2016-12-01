e1ed7dd5f5470fb78cfc78fd997dd029ec24d2a2
von-berlichingen <anis.b.amara@gmail.com>
2016-12-02 00:43:05 +0100

    gulp-5 Cleaning the build folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var clean = require('gulp-rimraf');
 
-gulp.task('default', [], function() {
+gulp.task('clean', [], function() {
+  console.log('Clean all files in build folder.');
+
+  return gulp
+    .src('build/*', { read: false })
+    .pipe(clean());
+});
+
+gulp.task('default', ['clean'], function() {
   console.log('Concating and moving all the css files in styles folder.');
-  gulp
+
+  return gulp
     .src('contents/styles/**.*')
     .pipe(concat('main.css'))
     .pipe(gulp.dest('build/styles'));
