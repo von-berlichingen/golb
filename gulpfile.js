65f98f867144d776e8a04eeb315d0c795ce51b7f
von-berlichingen <anis.b.amara@gmail.com>
2016-12-02 01:41:10 +0100

    gulp_6 Minify the main css file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var clean = require('gulp-rimraf');
+var cssmin = require('gulp-minify-css');
 
 gulp.task('clean', [], function() {
   console.log('Clean all files in build folder.');
@@ -11,10 +12,11 @@ gulp.task('clean', [], function() {
 });
 
 gulp.task('default', ['clean'], function() {
-  console.log('Concating and moving all the css files in styles folder.');
+  console.log('Concat, move and minify all css files in styles folder.');
 
   return gulp
-    .src('contents/styles/**.*')
-    .pipe(concat('main.css'))
+    .src('contents/styles/**.css')
+    .pipe(concat('main.min.css'))
+    .pipe(cssmin())
     .pipe(gulp.dest('build/styles'));
 });
